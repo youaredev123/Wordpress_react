4503fb4056c661ae882ee49e62ae5deb38b6e771
youaredev123 <youaredev123@gmail.com>
2022-03-02 18:18:47 +0300

    endpoints updated

---
+++ src/Routes.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Notes from "./containers/Notes";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Settings from "./containers/Settings";
+import NotFound from "./containers/NotFound";
+import ChangeEmail from "./containers/ChangeEmail";
+import ResetPassword from "./containers/ResetPassword";
+import ChangePassword from "./containers/ChangePassword";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/login/reset">
+        <ResetPassword />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings/email">
+        <ChangeEmail />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings/password">
+        <ChangePassword />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      {/* Finally, catch all unmatched routes */}
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
